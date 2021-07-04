be8d8925d295cbfdbebb95359c704de658557668
Julio Rodrigues <juliomarcosbortolon@gmail.com>
2021-07-04 23:11:49 +0100

    fake api

---
+++ pages/stories/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <code>{{ stories }}</code>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let stories
+    try {
+      stories = await $content('stories').fetch()
+    } catch (e) {
+      error({ message: 'stories not found' })
+    }
+    return { stories }
+  },
+  layout: 'api',
+}
+</script>
